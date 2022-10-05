2dfec315084bc7f90c5c17edbaaf2ca496fe1b73
FumiyaIwashige <fumiyaiwashige@gmail.com>
2022-10-05 13:45:59 +0900

    シーディング

---
+++ database/factories/SpendingFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Spending;
+use Faker\Generator as Faker;
+
+$factory->define(Spending::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
