0b17bd6dd8a3f0903e55fb117773df9794b3fbc5
FumiyaIwashige <fumiyaiwashige@gmail.com>
2022-10-31 11:05:55 +0900

    2022_1028

--- app/Http/Controllers/ShowController.php
+++ app/Http/Controllers/ShowController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Spending;
 use App\Category;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ShowController extends Controller
 {
@@ -13,14 +14,29 @@ class ShowController extends Controller
         return view('index')->with(['spendings' => $spending->get(), 'categories' => $category->get()]);
     }
     
-    public function show(Spending $spending)
-    {
-        return view('spendings/show')->with(['spendings' => $spending->get()]);
-    }
-    
+    /**
     public function show_edit(Request $request)
     {
         return view('spendings/show')->with(['spending_inf' => $request]);
     }
+    **/
+    public function show_edit(Spending $spending, Request $request)
+    {
+         return view('spendings/show')->with(['spending_inf' => $spending, 'date' => $request]);
+    }
     
+    public function update(Request $request, Spending $spending)
+    {
+        $input_spending = $request['spending'];
+        $spending->fill($input_spending)->save();
+        /**
+        $from = $request
+        **/
+        $data = DB::table('spendings')
+                ->whereBetween('updated_at', [$request['from'], $request['until']])
+                ->get();
+        dd($data);
+        
+        return redirect('/spendings/edit')->with(['date' => $request['date']]);
+    }
 }
